f61d7843903f51a03341e11d9bf47e801b59d412
Karen Etheridge <ether@cpan.org>
2021-04-30 17:54:53 -0700

    increment $VERSION after 1.200014 release

--- lib/Dist/Zilla/Plugin/InstallGuide.pm
+++ lib/Dist/Zilla/Plugin/InstallGuide.pm
@@ -5,7 +5,7 @@ use warnings;
 package Dist::Zilla::Plugin::InstallGuide;
 # ABSTRACT: Build an INSTALL file
 
-our $VERSION = '1.200014';
+our $VERSION = '1.200015';
 
 use Moose;
 with 'Dist::Zilla::Role::FileGatherer';
